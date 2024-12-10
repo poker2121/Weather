6035104c8f0b6e1b08e83f5b18e2cde03b9472c0
poker2121 <banoobnagy@gmail.com>
2024-12-10 19:30:03 +0200

    modify

--- js/index.js
+++ js/index.js
@@ -9,7 +9,9 @@ searchInput.addEventListener("input", function () {
 
 async function getWeather(city) {
   try {
-    const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=46530200f8cc4bbfa5d153529240812&q=${city}&days=3`);
+    const response = await fetch(
+      `https://api.weatherapi.com/v1/forecast.json?key=46530200f8cc4bbfa5d153529240812&q=${city}&days=3`
+    );
     const data = await response.json();
     displayWeather(data);
   } catch (error) {
@@ -32,8 +34,12 @@ function displayWeather(data) {
         <div class="card-body custom-card-body text-white">
           <p class="card-text pt-2">${location}</p>
           <h5 class="card-title bgfont">${forecast[0].day.avgtemp_c}°C</h5>
-          <div class="status-icon py-4"><img src="${forecast[0].day.condition.icon}" width="70" height="70" alt=""></div>
-          <div class="custom text-info pb-3">${forecast[0].day.condition.text}</div>
+          <div class="status-icon py-4"><img src="${
+            forecast[0].day.condition.icon
+          }" width="70" height="70" alt=""></div>
+          <div class="custom text-info pb-3">${
+            forecast[0].day.condition.text
+          }</div>
           <div class="icons-container gap-4">
             <div class="icon-with-text">
               <i class="fa-solid fa-umbrella"></i>
@@ -54,17 +60,13 @@ function displayWeather(data) {
   `;
 
   forecast.slice(1).forEach((day, index) => {
-    // تحديد الكروت باستخدام الفهرس
     let cardClass = "";
     let headerClass = "";
-    
-    // الكرت التاني
+
     if (index === 0) {
       headerClass = "hcard2";
       cardClass = "card2";
-    }
-    // باقي الكروت
-    else {
+    } else {
       headerClass = "custom-card-header";
       cardClass = "";
     }
@@ -72,12 +74,18 @@ function displayWeather(data) {
     cartoona += `
       <div class="col-md-4">
         <div class="card h-100 border-0 ${cardClass}">
-          <div class="card-header text-white ${headerClass} text-center rounded-0">${formatDay(day.date)}</div>
+          <div class="card-header text-white ${headerClass} text-center rounded-0">${formatDay(
+      day.date
+    )}</div>
           <div class="card-body custom-card-body text-white text-center ${cardClass} align-content-center">
-            <div class="status-icon pb-3"><img src="${day.day.condition.icon}" width="40" height="40" alt=""></div>
+            <div class="status-icon pb-3"><img src="${
+              day.day.condition.icon
+            }" width="45" height="45" alt=""></div>
             <h5 class="card-title pt-3">${day.day.avgtemp_c}°C</h5>
             <span class="text-white-50">${day.day.mintemp_c}°C</span>
-            <div class="custom text-info pb-3 py-4">${day.day.condition.text}</div>
+            <div class="custom text-info pb-3 py-4">${
+              day.day.condition.text
+            }</div>
           </div>
         </div>
       </div>
